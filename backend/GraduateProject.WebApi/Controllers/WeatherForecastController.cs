0d7fc2d91debc2d957726b223d1196efb93af293
thnam1410 <62019809+thnam1410@users.noreply.github.com>
2022-01-03 20:13:07 +0700

    First commit

---
+++ backend/GraduateProject.WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using GraduateProject.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraduateProject.Controllers;
+
+[ApiController]
+[Route("test")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+
+    [HttpGet("test")]
+    public async Task<ApiResponse<object>> TestAction()
+    {
+        return ApiResponse<object>.Ok("ok babi");
+    }
+}
---
+++ backend/GraduateProject.WebApi/Extensions/ApiResponse.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GraduateProject.Extensions;
+
+public class ApiResponse<TResult> : ApiResponse where TResult : class
+{
+    public TResult Result { get; }
+
+    private ApiResponse(
+        bool success,
+        string message,
+        TResult result,
+        object error = null,
+        int statusCode = 200)
+        : base(success, message, error, statusCode)
+    {
+        this.Result = result;
+    }
+
+    public static ApiResponse<TResult> Ok(TResult result = null, string message = null) => new ApiResponse<TResult>(true, message, result);
+
+    public static ApiResponse<TResult> Fail(
+        string message,
+        object errors = null,
+        int statusCode = 500,
+        TResult result = null)
+    {
+        return new ApiResponse<TResult>(false, message, result, errors, statusCode);
+    }
+}
+
+public class ApiResponse
+{
+    public bool Success { get; }
+
+    public string Message { get; }
+
+    public object Error { get; }
+
+    [JsonIgnore] public int StatusCode { get; }
+
+    protected ApiResponse(bool success, string message, object error = null, int statusCode = 200)
+    {
+        this.Message = message;
+        this.Error = error;
+        this.StatusCode = statusCode;
+        this.Success = success;
+    }
+
+    public static ApiResponse Ok(string message = null) => new ApiResponse(true, message);
+
+    public static ApiResponse Fail(string message, object errors = null, int statusCode = 500) => new ApiResponse(false, message, errors);
+}
