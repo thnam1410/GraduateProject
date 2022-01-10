b22c1041c71c1cda3c4877b6ca4fd604d3370968
thnam1410 <62019809+thnam1410@users.noreply.github.com>
2022-01-10 21:32:19 +0700

    convert minimal API to pure startup program format source

--- backend/GraduateProject.WebApi/Program.cs
+++ backend/GraduateProject.WebApi/Program.cs
@@ -1,36 +1,14 @@
-using GraduateProject.Infrastructure.Extensions;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-#region Configuration
-
-var connectionString = builder.Configuration.GetConnectionString("MY_DB");
-builder.Services.SetupInfrastructure(connectionString);
-
-
-#endregion
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace GraduateProject.WebApi
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ backend/GraduateProject.WebApi/Startup.cs
@@ -0,0 +1,51 @@
+﻿using GraduateProject.Infrastructure.Extensions;
+using Microsoft.OpenApi.Models;
+
+namespace GraduateProject;
+
+public class Startup
+{
+    
+    private readonly IHostEnvironment _env;
+    public static string connectionString { get; private set; }
+    public Startup(IConfiguration configuration, IHostEnvironment env)
+    {
+        Configuration = configuration;
+        _env = env;
+        connectionString = Configuration.GetConnectionString("My_DB");
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Graduation Project", Version = "v1"}); });
+        services.SetupInfrastructure(connectionString);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication1 v1");
+                c.EnableFilter();
+                c.DisplayRequestDuration();
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+}
