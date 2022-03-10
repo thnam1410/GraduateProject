f46af9316d39748febeda119586693297dcf89ab
Phạm Ngọc Danh <phamngocdanhhcm@gmail.com>
2022-03-10 16:56:10 +0700

    update login register

--- frontend/constants/apis/auth.api.ts
+++
@@ -1,6 +0,0 @@
-import { BASE_API_PATH } from "~/utils/ApiUtil";
-
-export const LOGIN_API = BASE_API_PATH + "/identity/login";
-export const REGISTER_API = BASE_API_PATH + "/identity/register";
-export const LOGOUT_API = BASE_API_PATH + "/identity/logout";
-export const CHECK_LOGIN_API = BASE_API_PATH + "/identity/check-login";
--- frontend/src/pages/auth/register.tsx
+++ frontend/src/pages/auth/register.tsx
@@ -7,14 +7,58 @@ import Link from "next/link";
 import Auth from "../../components/Auth";
 import Layout from "../../Layout";
 import Socials from "../../components/Auth/Socials";
+import { useRouter } from "next/router";
+import { ApiUtil } from "~/src/pages/utils/ApiUtil";
+import { ApiResponse } from "~/src/pages/types/api.type";
+
+import { LOGIN_API, REGISTER_API } from "~/src/constants/apis/auth.api";
 
 const Input = styled(InputGroup)`
 	margin-bottom: 2rem;
 `;
+interface IFormInput {
+	userName: string;
+	email: string;
+	phoneNumber: string;
+	password: string;
+	confirmPassword: string;
+	fullName: string;
+}
 
 export default function Register() {
+	const router = useRouter();
+
 	const onCheckbox = () => {
 		// v will be true or false
+	};
+	const onRedirectLogin = () => router.push("/login");
+	const onFinishFailed = (errorInfo: any) => {
+		console.log("Failed:", errorInfo);
+	};
+
+	const dataSource: IFormInput = {
+		userName: "Pham Ngoc Danh",
+		email: "check ",
+		phoneNumber: "0927140859",
+		password: "12345",
+		confirmPassword: "true",
+		fullName: "Pham Ngoc Danh",
+	};
+
+	const onFinish = (values: IFormInput) => {
+		ApiUtil.Axios.post<ApiResponse>(REGISTER_API, values)
+			.then((res) => {
+				if (res.data.success) {
+					ApiUtil.ToastSuccess("Đăng ký thành công! Vui lòng đăng nhập");
+					onRedirectLogin();
+				} else {
+					ApiUtil.ToastError("Đăng ký thất bại! Vui lòng thử lại");
+					console.log(res?.data?.message);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	};
 	return (
 		<Layout title="Đăng ký">
@@ -41,7 +85,7 @@ export default function Register() {
 					<Input fullWidth>
 						<input type="text" placeholder="Thành viên giới thiệu" />
 					</Input>
-					<Button status="Success" type="button" shape="SemiRound" fullWidth>
+					<Button onClick={() => onFinish(dataSource)} status="Success" type="button" shape="SemiRound" fullWidth>
 						Đăng ký
 					</Button>
 				</form>
