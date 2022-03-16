a0f5214a413dc3611fd60623133b7bcd1ceaf2c0
Phạm Ngọc Danh <phamngocdanhhcm@gmail.com>
2022-03-16 20:40:32 +0700

    update valid register

--- frontend/src/pages/auth/register.tsx
+++ frontend/src/pages/auth/register.tsx
@@ -5,6 +5,8 @@ import { ApiResponse } from "~/src/pages/types/api.type";
 
 import { REGISTER_API } from "~/src/constants/apis/auth.api";
 import TextArea from "antd/lib/input/TextArea";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
 interface IFormInput {
 	userName: string;
@@ -12,11 +14,17 @@ interface IFormInput {
 	phoneNumber: string;
 	password: string;
 	confirmPassword: string;
-	fullName: string;
+	decriptions: string;
+	referralMember: string;
 }
 
 export default function Register() {
 	const router = useRouter();
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm<IFormInput>();
 
 	const onCheckbox = () => {
 		// v will be true or false
@@ -26,16 +34,16 @@ export default function Register() {
 		console.log("Failed:", errorInfo);
 	};
 
-	const dataSource: IFormInput = {
-		userName: "Pham Ngoc Danh",
-		email: "check ",
-		phoneNumber: "0927140859",
-		password: "12345",
-		confirmPassword: "true",
-		fullName: "Pham Ngoc Danh",
-	};
+	// const dataSource: IFormInput = {
+	// 	userName: "Pham Ngoc Danh",
+	// 	email: "check ",
+	// 	phoneNumber: "0927140859",
+	// 	password: "12345",
+	// 	confirmPassword: "true",
+	// 	fullName: "Pham Ngoc Danh",
+	// };
 
-	const onFinish = (values: IFormInput) => {
+	const onFinish = handleSubmit((values: IFormInput) => {
 		ApiUtil.Axios.post<ApiResponse>(REGISTER_API, values)
 			.then((res) => {
 				if (res.data.success) {
@@ -49,7 +57,8 @@ export default function Register() {
 			.catch((err) => {
 				console.log(err);
 			});
-	};
+	});
+
 	return (
 		<div className="container mx-auto">
 			<div className="flex justify-center px-6 my-12">
@@ -60,40 +69,77 @@ export default function Register() {
 					></div>
 					<div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
 						<h3 className="pt-4 text-2xl text-center">Create an Account!</h3>
-						<form className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
-							<div className="mb-4 md:flex md:justify-between">
-								<div className="mb-4 md:mr-2 md:mb-0">
-									<label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="firstName">
-										Tên tài khoản
-									</label>
-									<input
-										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-										id="firstName"
-										type="text"
-										placeholder="First Name"
-									/>
-								</div>
-								<div className="md:ml-2">
-									<label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="lastName">
-										Địa chỉ Email
-									</label>
-									<input
-										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-										id="lastName"
-										type="text"
-										placeholder="Last Name"
-									/>
-								</div>
+						<form className="px-8 pt-6 pb-8 mb-4 bg-white rounded" onSubmit={onFinish}>
+							<div className="mb-4">
+								<label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="userName">
+									Tên tài khoản
+								</label>
+								<input
+									className={`w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline ${
+										errors?.userName && "border-red-500"
+									}`}
+									id="userName"
+									type="text"
+									placeholder="Tên tài khoản"
+									{...register("userName", {
+										required: {
+											value: true,
+											message: "Vui lòng nhập tên tài khoản",
+										},
+										minLength: {
+											value: 8,
+											message: "Ký tự không đủ",
+										},
+										maxLength: {
+											value: 120,
+											message: "Ký tự vượt quá ký tự cho phép",
+										},
+										pattern: {
+											value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+											message: "Vui lòng kiểm tra lại các ký tự không đúng cho tên tài khoản",
+										},
+									})}
+								/>
+								<ErrorMessage
+									errors={errors}
+									name="userName"
+									render={({ message }) => (message ? <p className="text-red-400">{message}</p> : null)}
+								/>
 							</div>
 							<div className="mb-4">
 								<label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="email">
 									Email
 								</label>
 								<input
-									className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									className={`w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline ${
+										errors?.email && "border-red-500"
+									}`}
 									id="email"
 									type="email"
 									placeholder="Email"
+									{...register("email", {
+										required: {
+											value: true,
+											message: "Vui lòng nhập email",
+										},
+										minLength: {
+											value: 8,
+											message: "Ký tự không đủ cho địa chỉ email",
+										},
+										maxLength: {
+											value: 120,
+											message: "Ký tự vượt quá ký tự cho phép dành cho địa chỉ email",
+										},
+										pattern: {
+											value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+											message: "Vui lòng kiểm tra lại các ký tự không đúng cho email",
+										},
+									})}
+								/>
+								<ErrorMessage
+									errors={errors}
+									name="email"
+									render={({ message }) => (message ? <p className="text-red-400">{message}</p> : null)}
 								/>
 							</div>
 							<div className="mb-4">
@@ -101,10 +147,31 @@ export default function Register() {
 									Số điện thoại
 								</label>
 								<input
-									className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									className={`w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline ${
+										errors?.phoneNumber && "border-red-500"
+									}`}
 									id="phoneNumber"
 									type="number"
 									placeholder="Số điện thoại"
+									{...register("phoneNumber", {
+										required: {
+											value: true,
+											message: "Vui lòng nhập số điện thoại",
+										},
+										minLength: {
+											value: 8,
+											message: "Số điện thoại không đủ số",
+										},
+										maxLength: {
+											value: 12,
+											message: "Số điện thoại quá dài",
+										},
+									})}
+								/>
+								<ErrorMessage
+									errors={errors}
+									name="phoneNumber"
+									render={({ message }) => (message ? <p className="text-red-400">{message}</p> : null)}
 								/>
 							</div>
 							<div className="mb-4">
@@ -132,34 +199,67 @@ export default function Register() {
 							<div className="mb-4 md:flex md:justify-between">
 								<div className="mb-4 md:mr-2 md:mb-0">
 									<label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="password">
-										Password
+										Mật khẩu
 									</label>
 									<input
-										className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										className={`w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline ${
+											errors?.password && "border-red-500"
+										}`}
 										id="password"
 										type="password"
 										placeholder="******************"
+										{...register("password", {
+											required: {
+												value: true,
+												message: "Vui lòng nhập mật khẩu",
+											},
+											minLength: {
+												value: 8,
+												message: "Mật khẩu quá ngắn",
+											},
+										})}
 									/>
-									<p className="text-xs italic text-red-500">Mật khẩu.</p>
+									<ErrorMessage
+										errors={errors}
+										name="password"
+										render={({ message }) => (message ? <p className="text-red-400">{message}</p> : null)}
+									/>{" "}
 								</div>
 								<div className="md:ml-2">
-									<label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="c_password">
+									<label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="confirmPassword">
 										Xác nhận mật khẩu
 									</label>
 									<input
-										className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-										id="c_password"
+										className={`w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline ${
+											errors?.confirmPassword && "border-red-500"
+										}`}
+										id="confirmPassword"
 										type="password"
 										placeholder="******************"
+										{...register("confirmPassword", {
+											required: {
+												value: true,
+												message: "Vui lòng nhập mật khẩu xác nhận",
+											},
+											minLength: {
+												value: 8,
+												message: "Mật khẩu xác nhận quá ngắn",
+											},
+										})}
+									/>
+									<ErrorMessage
+										errors={errors}
+										name="confirmPassword"
+										render={({ message }) => (message ? <p className="text-red-400">{message}</p> : null)}
 									/>
 								</div>
 							</div>
 							<div className="mb-6 text-center">
 								<button
 									className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-									type="button"
+									type="submit"
 								>
-									Register Account
+									Đăng ký
 								</button>
 							</div>
 							<hr className="mb-6 border-t" />
