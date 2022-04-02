2c86a8b10a0e2bcdc8ee2cbd9f9eca3c0e5af7cb
thnam1410 <62019809+thnam1410@users.noreply.github.com>
2022-04-02 18:35:44 +0700

    update

--- frontend/src/components/FormFields/BaseForm.tsx
+++ frontend/src/components/FormFields/BaseForm.tsx
@@ -1,24 +1,47 @@
-import React, { createElement, ReactElement } from "react";
+import React, { createElement, ReactElement, useImperativeHandle } from "react";
 import { DeepPartial, SubmitHandler, useForm, FormProvider } from "react-hook-form";
 import { DefaultValues } from "react-hook-form/dist/types/form";
-import {Resolver} from "react-hook-form/dist/types/resolvers";
+import { Resolver } from "react-hook-form/dist/types/resolvers";
 
-interface IFormProps<T> {
-	onSubmit: SubmitHandler<T>;
-	defaultValues?: DefaultValues<T>;
+interface IFormProps {
+	onSubmit: (formValues: any) => void;
+	defaultValues?: DefaultValues<any>;
 	className?: string;
-	resolver?: Resolver<T>;
+	resolver?: Resolver<any>;
+}
+export interface BaseFormRef {
+	getValues: () => Record<string, any>;
+	setValue: (key: string, value: any) => void;
+	setValues: (records: Record<string, any>) => void;
 }
 
-export function BaseForm<T>(props: IFormProps<T> & { children: ReactElement | ReactElement[] }) {
+type IProps<T> = IFormProps & { children: ReactElement | ReactElement[] };
+type IForwardRef<T> = React.ForwardedRef<BaseFormRef>;
+
+function BaseForm<T>(props: IProps<T>, ref: IForwardRef<T>) {
 	const { defaultValues, children, onSubmit, className, resolver } = props;
-	const methods  = useForm<T>({ defaultValues, resolver });
-	const { handleSubmit } = methods
+	const methods = useForm({ defaultValues, resolver });
+	const { handleSubmit, getValues, setValue, reset } = methods;
+
+	useImperativeHandle(
+		ref,
+		() => ({
+			getValues,
+			setValue,
+			setValues: reset
+		}),
+		[getValues, reset, setValue]
+	);
+
 	return (
 		<FormProvider {...methods}>
+			{/*<button onClick={() => console.log('getValues()',getValues())}>Inside</button>*/}
+			{/*<button onClick={() => console.log('getValues()',setValue('masterKey', 'test'))}>InsideSet</button>*/}
 			<form className={className} onSubmit={handleSubmit(onSubmit)}>
 				{children}
 			</form>
 		</FormProvider>
 	);
 }
+
+export default React.forwardRef(BaseForm);
--- frontend/src/components/pages/admin/masterdata/MasterData.Form.tsx
+++ frontend/src/components/pages/admin/masterdata/MasterData.Form.tsx
@@ -1,15 +1,12 @@
-import React from "react";
+import React, { useRef } from "react";
 import TextField from "~/src/components/FormFields/TextField";
-import { Col, Form, Input, Row } from "antd";
+import { Col, Form, Row } from "antd";
 import ButtonBase from "~/src/components/ButtonBase/ButtonBase";
-import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { Controller } from "react-hook-form";
-import { MasterData } from "~/src/types/MasterData";
+import BaseForm, { BaseFormRef } from "~/src/components/FormFields/BaseForm";
 import { ApiUtil } from "~/src/utils/ApiUtil";
-import { MASTER_DATA_CREATE_API, MASTER_DATA_INDEX_API, MASTER_DATA_UPDATE_API } from "~/src/constants/apis/masterdata.api";
-import { BaseForm } from "~/src/components/FormFields/BaseForm";
+import { MASTER_DATA_CREATE_API, MASTER_DATA_UPDATE_API } from "~/src/constants/apis/masterdata.api";
 
 interface IProps {
 	onClose: () => void;
@@ -31,46 +28,33 @@ const schema = yup
 	.required();
 
 const MasterDataForm: React.FC<IProps> = (props) => {
-	const { initData, onClose } = props;
-	const {
-		formState: { errors },
-	} = useForm<IFormValues>({
-		resolver: yupResolver(schema),
-		defaultValues: initData
-			? {
-					id: initData.id,
-					code: initData.code,
-					masterKey: initData.masterKey,
-					name: initData.name,
-			  }
-			: {},
-	});
+	const formRef = useRef<BaseFormRef>(null);
 	const onSubmit = async (formValues: IFormValues) => {
 		const isUpdate = formValues.id ? true : false;
 		const result = await ApiUtil.Axios.post(isUpdate ? MASTER_DATA_UPDATE_API : MASTER_DATA_CREATE_API, formValues);
-		onClose();
+		props.onClose();
 	};
 	return (
 		<div>
-			<BaseForm className="p-3 pb-2" onSubmit={onSubmit} resolver={yupResolver(schema)} defaultValues={props.initData as IFormValues}>
+			<BaseForm ref={formRef} className="p-3 pb-2" onSubmit={onSubmit} resolver={yupResolver(schema)} defaultValues={props.initData}>
 				<Row className="m-2">
 					<Col span={24}>
 						<Form.Item>
-							<TextField name={"masterKey"} errors={errors} label={"Master Key"} />
+							<TextField name={"masterKey"} label={"Master Key"} />
 						</Form.Item>
 					</Col>
 				</Row>
 				<Row className="m-2">
 					<Col span={24}>
 						<Form.Item>
-							<TextField name={"code"} errors={errors} label={"Mã"} />
+							<TextField name={"code"} label={"Mã"} />
 						</Form.Item>
 					</Col>
 				</Row>
 				<Row className="m-2">
 					<Col span={24}>
 						<Form.Item>
-							<TextField name={"name"} errors={errors} label={"Tên"} />
+							<TextField name={"name"} label={"Tên"} />
 						</Form.Item>
 					</Col>
 				</Row>
