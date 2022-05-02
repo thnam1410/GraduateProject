4f545742481c6055146632bd93a4b2af56e23d4c
Phạm Ngọc Danh <phamngocdanhhcm@gmail.com>
2022-05-02 16:27:21 +0700

    Update Map.tsx

--- frontend/src/components/layout/Map.tsx
+++ frontend/src/components/layout/Map.tsx
@@ -8,11 +8,13 @@ import { GetServerSideProps, NextPage } from "next";
 import { useLeafletContext } from "@react-leaflet/core";
 import { LatLngBoundsExpression, Map as LeafletMap, Polyline as LeafletPolyline } from "leaflet";
 import { ApiUtil, BASE_API_PATH } from "~/src/utils/ApiUtil";
+import GooglePlacesAutocomplete, { geocodeByAddress, geocodeByPlaceId, getLatLng } from "react-google-places-autocomplete";
 
 const Map: NextPage<any> = ({ children }) => {
 	const polyLineRef = useRef<LeafletPolyline>(null);
 	const leafletMap = useRef<LeafletMap>(null);
 	const [positions, setPosition] = useState<any[]>([]);
+	const [address, setAddress] = useState<any>(null);
 
 	useEffect(() => {
 		ApiUtil.Axios.get(BASE_API_PATH + "/route/get-path-by-route-detail-id?routeDetailId=1651").then((res) => {
@@ -24,7 +26,13 @@ const Map: NextPage<any> = ({ children }) => {
 			setPosition(positionResult);
 			setTimeout(() => leafletMap.current?.fitBounds(polyLineRef.current?.getBounds() as LatLngBoundsExpression));
 		});
-	}, []);
+		const getInfoPlaceId = async () => {
+			// console.log("address", address);
+			const geocodeObj = address && address.value && (await geocodeByPlaceId(address.value.place_id));
+			// console.log("geocodeObj", geocodeObj);
+		};
+		getInfoPlaceId();
+	}, [address]);
 
 	const renderMarkers = () => {
 		return (
@@ -52,40 +60,43 @@ const Map: NextPage<any> = ({ children }) => {
 	};
 
 	const renderRoutes = () => {
-		return (
-			<Polyline
-				ref={polyLineRef}
-				pathOptions={{ color: "purple" }}
-				// positions={[
-				// 	[10.75104046, 106.65258026],
-				// 	[10.75094986, 106.65097046],
-				// 	[10.75100994, 106.65048218],
-				// 	[10.7514801, 106.65052795],
-				// 	[10.75234032, 106.65061951],
-				// 	[10.75352001, 106.65074158],
-				// ]}
-				positions={positions}
-			/>
-		);
+		return <Polyline ref={polyLineRef} pathOptions={{ color: "purple" }} positions={positions} />;
 	};
 	return (
 		<>
-			<div style={{ height: 50, width: "100%" }}>
-				{/*<button*/}
-				{/*	onClick={() => {*/}
-				{/*		console.log("leafletMap.current", leafletMap.current);*/}
-				{/*		leafletMap.current?.fitBounds(polyLineRef.current?.getBounds() as LatLngBoundsExpression);*/}
-				{/*	}}*/}
-				{/*>*/}
-				{/*	Click*/}
-				{/*</button>*/}
+			<div
+				style={{
+					position: "absolute",
+					zIndex: 3,
+					width: "40%",
+					left: "60px",
+					top: "10px",
+				}}
+			>
+				{
+					<GooglePlacesAutocomplete
+						apiKey="AIzaSyDQXEJEdPF6r1WQMhj9rWW03oyV39kh0Dg"
+						selectProps={{
+							placeholder: "Nhập vị trí cần chọn",
+							isClearable: true,
+							value: address,
+							onChange: (val: any) => {
+								setAddress(val);
+							},
+						}}
+					/>
+				}
 			</div>
 			<MapContainer
 				ref={leafletMap}
 				center={[10.762622, 106.660172]}
 				zoom={14}
 				scrollWheelZoom={true}
-				style={{ height: "100%", width: "100%" }}
+				style={{
+					position: "relative",
+					zIndex: 1,
+					height: "100%",
+				}}
 			>
 				<TileLayer
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
