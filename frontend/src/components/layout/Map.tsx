0a1b3d6978bb66e41091343469e30a5a81a91801
Phạm Ngọc Danh <phamngocdanhhcm@gmail.com>
2022-05-05 20:38:32 +0700

    Update Map.tsx

--- frontend/src/components/layout/Map.tsx
+++ frontend/src/components/layout/Map.tsx
@@ -17,6 +17,8 @@ const Map: NextPage<any> = ({ children }) => {
 	const leafletMap = useRef<LeafletMap>(null);
 	const [positions, setPosition] = useState<any[]>([]);
 	const [address, setAddress] = useState<any>(null);
+	const [openTab, setOpenTab] = useState<number>(1);
+
 	const { TabPane } = Tabs;
 
 	useEffect(() => {
@@ -71,10 +73,61 @@ const Map: NextPage<any> = ({ children }) => {
 	return (
 		<>
 			<div className="border bg-white-800 absolute duration-500 -left-0 " style={{ width: "27%" }}>
-				<Tabs className="flex" defaultActiveKey="1" onChange={() => {}}>
-					<TabPane className="flex-1" tab="Tab 1" key="1"></TabPane>
-					<TabPane className="flex-1" tab="Tab 2" key="2"></TabPane>
-				</Tabs>
+				<div className="flex flex-wrap">
+					<div className="w-full">
+						<ul className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row" role="tablist">
+							<li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+								<a
+									className={
+										"text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+										(openTab === 1 ? "text-white bg-blue-600" : "text-blue-600 bg-white")
+									}
+									onClick={(e) => {
+										e.preventDefault();
+										setOpenTab(1);
+									}}
+									data-toggle="tab"
+									href="#link1"
+									role="tablist"
+								>
+									Tra cứu
+								</a>
+							</li>
+							<li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+								<a
+									className={
+										"text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+										(openTab === 2 ? "text-white bg-blue-600" : "text-blue-600 bg-white")
+									}
+									onClick={(e) => {
+										e.preventDefault();
+										setOpenTab(2);
+									}}
+									data-toggle="tab"
+									href="#link2"
+									role="tablist"
+								>
+									Tìm đường
+								</a>
+							</li>
+						</ul>
+						<div
+							className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded"
+							style={{ height: 800 }}
+						>
+							<div className="px-4 py-5 flex-auto">
+								<div className="tab-content tab-space">
+									<div className={openTab === 1 ? "block" : "hidden"} id="link1">
+										<p>v</p>
+									</div>
+									<div className={openTab === 2 ? "block" : "hidden"} id="link2">
+										<p>a</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 
 			<div
