29b1ee83beeaae7a05bdb271fb536b705cdb3d33
Phạm Ngọc Danh <phamngocdanhhcm@gmail.com>
2022-05-30 20:32:37 +0700

    update

--- frontend/src/components/layout/HeaderView.tsx
+++ frontend/src/components/layout/HeaderView.tsx
@@ -2,7 +2,6 @@ import { GetServerSideProps, NextPage } from "next";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { UserSession } from "../../types/UserInfo";
-
 import {
 	PathIconBusHeader1,
 	PathIconBusHeader2,
@@ -11,18 +10,68 @@ import {
 	PathIconBusHeader5,
 	PathIconBusHeader6,
 } from "../pages/svg/Path";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useStore } from "~/src/zustand/store";
 const HeaderView: NextPage<any> = ({ children }) => {
 	const userSession = useStore((state) => state.userSession);
-
+	const [showOptions, setShowOptions] = useState<boolean>(false);
 	useEffect(() => {}, [userSession]);
+	const handleClick = () => {
+		setShowOptions(!showOptions);
+	};
 
 	const renderRightLogin = () => {
 		return (
 			<>
 				{userSession !== null ? (
-					"Đã login"
+					<>
+						<div className="mt-3 mr-4 relative inline-block text-left">
+							<div>
+								<button
+									onClick={handleClick}
+									type="button"
+									className=" inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+									id="menu-button"
+									aria-expanded="true"
+									aria-haspopup="true"
+								>
+									{userSession?.user?.fullName}
+									<svg
+										className="-mr-1 ml-2 h-5 w-5"
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 20 20"
+										fill="currentColor"
+										aria-hidden="true"
+									></svg>
+								</button>
+							</div>
+
+							{showOptions && (
+								<div
+									className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+									style={{ zIndex: 3 }}
+									role="menu"
+									aria-orientation="vertical"
+									aria-labelledby="menu-button"
+									tabIndex={-1}
+								>
+									<div className="py-1" role="none">
+										<form method="POST" action="#" role="none">
+											<button
+												type="submit"
+												className="text-gray-700 block w-full text-left px-4 py-2 text-sm"
+												role="menuitem"
+												tabIndex={-1}
+												id="menu-item-3"
+											>
+												Đăng xuất
+											</button>
+										</form>
+									</div>
+								</div>
+							)}
+						</div>
+					</>
 				) : (
 					<>
 						<Link href={"/auth/login"}>
