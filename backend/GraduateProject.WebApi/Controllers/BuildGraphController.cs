3f18f53115af2fcdbdca410da0aa1d9ff3783d81
thnam1410 <62019809+thnam1410@users.noreply.github.com>
2022-06-14 22:14:42 +0700

    update crawler + buildgraph v2

---
+++ backend/GraduateProject.WebApi/Controllers/BuildGraphController.cs
@@ -0,0 +1,155 @@
+﻿using GraduateProject.Application.Common.Dto;
+using GraduateProject.Application.Extensions;
+using GraduateProject.Domain.AppEntities.Entities;
+using GraduateProject.Domain.AppEntities.Repositories;
+using GraduateProject.Domain.Common;
+using GraduateProject.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraduateProject.Controllers;
+
+public class BuildGraphController: ControllerBase
+{
+    private readonly IRouteDetailRepository _routeDetailRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IVertexRepository _vertexRepository;
+    private readonly ILogger<BuildGraphController> _logger;
+
+    public BuildGraphController(IRouteDetailRepository routeDetailRepository, IUnitOfWork unitOfWork, IVertexRepository vertexRepository, ILogger<BuildGraphController> logger)
+    {
+        _routeDetailRepository = routeDetailRepository;
+        _unitOfWork = unitOfWork;
+        _vertexRepository = vertexRepository;
+        _logger = logger;
+    }
+
+
+    [HttpGet("build-graph")]
+    public async Task<ApiResponse> HandleBuildGraph()
+    {
+        var routeDetails = await _routeDetailRepository.Queryable().Include(x => x.Vertices).ToListAsync();
+        var listVertex = new List<Edge>();
+        foreach (var routeDetail in routeDetails)
+        {
+            var listPaths = routeDetail.Vertices.OrderBy(x => x.Rank).ToList();
+            if (listPaths.Count() < 2) continue;
+            Position startPoint = null;
+            Guid pathId = default;
+            for (int i = 0; i < listPaths.Count() - 1; i++)
+            {
+                if (startPoint is null)
+                {
+                    startPoint = new Position() {Lat = listPaths[i].Lat, Lng = listPaths[i].Lng};
+                    pathId = listPaths[i].Id;
+                }
+
+                var pointB = listPaths[i + 1];
+                var positionB = new Position() {Lat = pointB.Lat, Lng = pointB.Lng};
+                var distance = CalculateUtil.Distance(startPoint, positionB);
+                if (distance != 0)
+                {
+                    listVertex.Add(new Edge()
+                    {
+                        PointAId = pathId,
+                        PointBId = pointB.Id,
+                        Distance = distance,
+                        ParentRouteDetailId = routeDetail.Id,
+                        Type = EdgeType.MainRoute
+                    });
+                    startPoint = null;
+                    pathId = default;
+                }
+            }
+        }
+
+        try
+        {
+            await _unitOfWork.BeginTransactionAsync();
+            await _vertexRepository.AddEdgeList(listVertex, true);
+            await _unitOfWork.CommitTransactionAsync();
+            return ApiResponse.Ok();
+        }
+        catch (Exception e)
+        {
+            await _unitOfWork.RollBackTransactionAsync();
+            return ApiResponse.Fail(e.Message);
+        }
+    }
+    
+    
+    
+    [HttpGet("build-connect-graph")]
+    public async Task<ApiResponse> HandleBuildConnectGraph()
+    {
+        double litmitDistance = 0.3; //300m
+        var vertices = await _vertexRepository.Queryable().AsNoTracking()
+            .Select(x => new EdgeSimpleDto
+            {
+                Id = x.Id, Lat = x.Lat, Lng = x.Lng, RouteId = x.RouteDetail.RouteId
+            })
+            .ToListAsync();
+        var switchEdges = new List<Edge>();
+        var total = vertices.Count;
+        foreach (var (currentVertex, idx) in vertices.WithIndex())
+        {
+            _logger.LogInformation("Loop item left: " + (total - idx));
+            var pointA = new Position() {Lat = currentVertex.Lat, Lng = currentVertex.Lng};
+            var nearestVertex = vertices
+                .Where(loopItemVertex => loopItemVertex.RouteId != currentVertex.RouteId &&
+                                         CalculateUtil.Distance(pointA, new Position() {Lng = loopItemVertex.Lng, Lat = loopItemVertex.Lat}) <= litmitDistance).Distinct()
+                .ToList();
+            if (nearestVertex.Any())
+            {
+                foreach (var groupByRouteItem in nearestVertex.GroupBy(x => x.RouteId))
+                {
+                    var items = groupByRouteItem.ToList();
+                    double min = double.MaxValue;
+                    EdgeSimpleDto minItem = null;
+                    foreach (var item in items)
+                    {
+                        var distance = CalculateUtil.Distance(pointA, new Position() {Lng = item.Lng, Lat = item.Lat});
+                        if (distance > 0 && distance < min)
+                        {
+                            min = distance;
+                            minItem = item;
+                        }
+                    }
+
+                    if (minItem is not null)
+                    {
+                        var pointB = new Position() {Lat = minItem.Lat, Lng = minItem.Lng};
+                        switchEdges.Add(new Edge()
+                        {
+                            PointAId = currentVertex.Id,
+                            PointBId = minItem.Id,
+                            Distance = CalculateUtil.Distance(pointA, pointB),
+                            Type = EdgeType.SwitchRoute,
+                        });
+                    }
+                }
+            }
+        }
+
+        try
+        {
+            await _unitOfWork.BeginTransactionAsync();
+            await _vertexRepository.BulkInsertEdgeList(switchEdges);
+            await _unitOfWork.CommitTransactionAsync();
+            return ApiResponse.Ok();
+        }
+        catch (Exception e)
+        {
+            await _unitOfWork.RollBackTransactionAsync();
+            return ApiResponse.Fail(e.Message);
+        }
+    }
+    
+    private class EdgeSimpleDto
+    {
+        public Guid Id { get; set; }
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+        public int RouteId { get; set; }
+    }
+}
--- backend/GraduateProject.WebApi/Controllers/CrawlControllerV2.cs
+++ backend/GraduateProject.WebApi/Controllers/CrawlControllerV2.cs
@@ -5,6 +5,8 @@ using GraduateProject.Domain.AppEntities.Repositories;
 using GraduateProject.Domain.Common;
 using GraduateProject.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Route = GraduateProject.Domain.AppEntities.Entities.Route;
 
 namespace GraduateProject.Controllers;
 
@@ -14,13 +16,22 @@ public class CrawlControllerV2 : ControllerBase
     private readonly IObjectMapper _mapper;
     private readonly IUnitOfWork _unitOfWork;
     private readonly ICrawlEntityRepository _crawlEntityRepository;
+    private readonly IStopRepository _stopRepository;
+    private readonly IRouteRepository _routeRepository;
+    private readonly IVertexRepository _vertexRepository;
+    private readonly IRouteDetailRepository _routeDetailRepository;
 
-    public CrawlControllerV2(ILogger<CrawlerController> logger, IObjectMapper mapper, IUnitOfWork unitOfWork, ICrawlEntityRepository crawlEntityRepository)
+    public CrawlControllerV2(ILogger<CrawlerController> logger, IObjectMapper mapper, IUnitOfWork unitOfWork, ICrawlEntityRepository crawlEntityRepository,
+        IStopRepository stopRepository, IRouteRepository routeRepository, IVertexRepository vertexRepository, IRouteDetailRepository routeDetailRepository)
     {
         _logger = logger;
         _mapper = mapper;
         _unitOfWork = unitOfWork;
         _crawlEntityRepository = crawlEntityRepository;
+        _stopRepository = stopRepository;
+        _routeRepository = routeRepository;
+        _vertexRepository = vertexRepository;
+        _routeDetailRepository = routeDetailRepository;
     }
 
     [HttpGet("start")]
@@ -37,8 +48,8 @@ public class CrawlControllerV2 : ControllerBase
         {
             var resRouteInfo = await client.GetAsync($"http://apicms.ebms.vn/businfo/getroutebyid/{routeId}");
             var resultRouteInfo = await resRouteInfo.Content.ReadFromJsonAsync<CrawlRoute>();
-            if(resultRouteInfo is not null) listRoutes.Add(resultRouteInfo);
-            
+            if (resultRouteInfo is not null) listRoutes.Add(resultRouteInfo);
+
             var resUrl = await client.GetAsync($"http://apicms.ebms.vn/businfo/getvarsbyroute/{routeId}");
             var resultRouteDetail = await resUrl.Content.ReadFromJsonAsync<List<CrawlRouteDetail>>();
             if (resultRouteDetail is not null && resultRouteDetail.Any())
@@ -66,6 +77,7 @@ public class CrawlControllerV2 : ControllerBase
                             stopData.ForEach(stop => stop.RouteVarId = routeVarId);
                             listStops.AddRange(stopData);
                         }
+
                         if (pathData is not null)
                         {
                             pathData.RouteId = routeId;
@@ -80,6 +92,7 @@ public class CrawlControllerV2 : ControllerBase
                 }
             }
         }
+
         var listCrawlEntityRoutes = listRoutes;
         var listCrawlEntityRouteDetail = listRouteDetails;
         var listCrawlEntityStops = listStops.GroupBy(x => x.AddressNo).Select(x => x.First()).ToList();
@@ -98,22 +111,22 @@ public class CrawlControllerV2 : ControllerBase
                 });
             }
         }
-        
+
         try
         {
             await _unitOfWork.BeginTransactionAsync();
             await _crawlEntityRepository.AddRangeCrawlRouteAsync(listCrawlEntityRoutes);
             _logger.LogInformation("Crawl Routes into DB successfully!");
-            
+
             await _crawlEntityRepository.AddRangeCrawlRouteDetailAsync(listCrawlEntityRouteDetail);
             _logger.LogInformation("Crawl Routes Detail into DB successfully!");
-            
+
             await _crawlEntityRepository.AddRangeCrawlPathAsync(listCrawlEntityPath);
             _logger.LogInformation("Crawl Path into DB successfully!");
-            
+
             await _crawlEntityRepository.AddRangeCrawlStopAsync(listCrawlEntityStops);
             _logger.LogInformation("Crawl Stops into DB successfully!");
-            
+
             await _unitOfWork.CommitTransactionAsync();
         }
         catch (Exception e)
@@ -121,7 +134,207 @@ public class CrawlControllerV2 : ControllerBase
             await _unitOfWork.RollBackTransactionAsync();
             return ApiResponse.Fail(e.Message);
         }
+
         return ApiResponse.Ok();
+    }
+
+
+    [HttpGet("trigger-preprocess-data-stop-1")]
+    public async Task<ApiResponse> HandlePreprocessDataStop()
+    {
+        try
+        {
+            await _unitOfWork.BeginTransactionAsync();
+            List<Stop> stopEntities = await PreprocessStopEntities();
+            await _stopRepository.AddRangeAsync(stopEntities, true);
+            await _unitOfWork.CommitTransactionAsync();
+        }
+        catch (Exception e)
+        {
+            await _unitOfWork.RollBackTransactionAsync();
+        }
+
+        return ApiResponse.Ok();
+    }
+
+    [HttpGet("trigger-preprocess-data-route-2")]
+    public async Task<ApiResponse> HandlePreprocessDataRoute()
+    {
+        try
+        {
+            await _unitOfWork.BeginTransactionAsync();
+            List<Route> routeEntities = await PreprocessRouteEntities();
+            await _routeRepository.UpdateIdentityInsert(true);
+            await _routeRepository.AddRangeAsync(routeEntities, true);
+            await _routeRepository.UpdateIdentityInsert(false);
+            await _unitOfWork.CommitTransactionAsync();
+        }
+        catch (Exception e)
+        {
+            await _unitOfWork.RollBackTransactionAsync();
+            return ApiResponse.Fail(e.Message);
+        }
+
+        return ApiResponse.Ok();
+    }
+    
+    [HttpGet("trigger-preprocess-data-path-3")]
+    public async Task<ApiResponse> HandlePreprocessDataPath()
+    {
+        try
+        {
+            await _unitOfWork.BeginTransactionAsync();
+            List<Vertex> pathEntities = await PreprocessPathEntities();
+            await _vertexRepository.AddRangeAsync(pathEntities, true);
+            await _unitOfWork.CommitTransactionAsync();
+        }
+        catch (Exception e)
+        {
+            await _unitOfWork.RollBackTransactionAsync();
+        }
+
+        return ApiResponse.Ok();
+    }
+    
+    [HttpGet("trigger-preprocess-data-stop-route-4")]
+    public async Task<ApiResponse> HandlePreprocessStopRoute()
+    {
+        try
+        {
+            await _unitOfWork.BeginTransactionAsync();
+            var routeDetailEntities = await _routeDetailRepository.Queryable().AsNoTracking()
+                .Include(x => x.Route).ToListAsync();
+            List<Stop> stopEntities = await _stopRepository.Queryable().ToListAsync();
+            foreach (var stop in stopEntities)
+            {
+                var listRouteCode = stop.Routes.Split(",").Select(x => x.Trim());
+                foreach (var routeCode in listRouteCode)
+                {
+                    var routeDetail = routeDetailEntities.FirstOrDefault(x => string.Equals(x.Route.RouteCode, routeCode, StringComparison.CurrentCultureIgnoreCase) && x.RouteVarId == stop.RouteVarId);
+                    if (routeDetail is not null && routeDetail.RouteStops.All(x => x.StopId != stop.Id && x.RouteDetailId != routeDetail.Id))
+                    {
+                        stop.RouteStops.Add(new RouteStop()
+                        {
+                            RouteDetailId = routeDetail.Id,
+                            StopId = stop.Id,
+                        });
+                    }
+                }
+            }
+
+            await _stopRepository.UpdateRangeAsync(stopEntities, true);
+            await _unitOfWork.CommitTransactionAsync();
+            return ApiResponse.Ok();
+        }
+        catch (Exception e)
+        {
+            await _unitOfWork.RollBackTransactionAsync();
+            return ApiResponse.Fail(e.Message);
+        }
+    }
+    
+    private async Task<List<Vertex>> PreprocessPathEntities()
+    {
+        var routeDetails = await _routeDetailRepository.Queryable().AsNoTracking().Select(x => new {x.Id, x.RouteId, x.RouteVarId}).ToListAsync();
+        var vertices = (await _crawlEntityRepository.GenericQueryable<CrawlPath>().AsNoTracking().ToListAsync())
+            .Select(x => new Vertex()
+            {
+                Lat = x.Lat,
+                Lng = x.Lng,
+                RouteDetailId = routeDetails.First(y => y.RouteId == x.RouteId && y.RouteVarId == x.RouteVarId).Id,
+                Rank = x.Rank
+            }).ToList();
+        var duplicateItems = new List<Vertex>();
+        foreach (var group in vertices.GroupBy(x => x.RouteDetailId))
+        {
+            var groupItems = group.ToList();
+            groupItems.ForEach(item =>
+            {
+                if (duplicateItems.Contains(item)) return;
+                var dupItems = vertices.Where(x => x.Lng.Equals(item.Lng) && x.Lat.Equals(item.Lat) && x.RouteDetailId == item.RouteDetailId).ToList();
+                if (dupItems.Count >= 1)
+                {
+                    dupItems.RemoveAt(0);
+                    duplicateItems.AddRange(dupItems);
+                }
+            });
+        }
+
+        if (duplicateItems.Any())
+        {
+            vertices.RemoveAll(vertex => duplicateItems.Contains(vertex));
+        }
+
+        foreach (var group in vertices.GroupBy(x => x.RouteDetailId))
+        {
+            var groupItems = group.ToList();
+            foreach (var (vertex, index) in groupItems.OrderBy(x => x.Rank).WithIndex())
+            {
+                vertex.Rank = index + 1;
+            }
+        }
+        
+        return vertices;
+    }
+    
+
+    private async Task<List<Route>> PreprocessRouteEntities()
+    {
+        var routes = await _crawlEntityRepository.GenericQueryable<CrawlRoute>().AsNoTracking().ToListAsync();
+        var routeDetails = await _crawlEntityRepository.GenericQueryable<CrawlRouteDetail>().AsNoTracking().ToListAsync();
+        var routeEntities = new List<Route>();
+        foreach (var route in routes)
+        {
+            routeEntities.Add(new Route()
+            {
+                Id = route.RouteId,
+                Name = route.RouteName,
+                Type = route.Type,
+                TimeRange = route.OperationTime,
+                Unit = route.Orgs,
+                RouteCode = route.RouteNo,
+                BusType = Math.Round(route.Distance / 1000, 2).ToString(),
+                RouteRange = route.NumOfSeats,
+                RouteDetails = routeDetails
+                    .Where(x => x.RouteId == route.RouteId)
+                    .Select(x => new RouteDetail()
+                    {
+                        RouteVarId = x.RouteVarId,
+                        RouteVarName = x.RouteVarName,
+                        RouteNo = x.RouteNo,
+                        Distance = Math.Round(route.Distance / 1000, 2),
+                        EndStop = x.EndStop,
+                        Outbound = x.Outbound,
+                        RouteVarShortName = x.RouteVarShortName,
+                        RunningTime = x.RunningTime,
+                        StartStop = x.StartStop,
+                        RouteId = x.RouteId,
+                    }).ToList()
+            });
+        }
+
+        return routeEntities;
+    }
+
+    private async Task<List<Stop>> PreprocessStopEntities()
+    {
+        var stopEntities = await _crawlEntityRepository.GenericQueryable<CrawlStop>()
+            .AsNoTracking()
+            .Select(x => new Stop()
+            {
+                Name = x.Name,
+                AddressNo = x.AddressNo,
+                Code = x.Code,
+                Lat = x.Lat,
+                Lng = x.Lng,
+                Routes = x.Routes,
+                Search = x.Search,
+                Status = x.Status,
+                StopType = x.StopType,
+                Street = x.Street,
+                RouteVarId = x.RouteVarId
+            }).ToListAsync();
+        return stopEntities;
     }
 
     private static async Task<List<int>> ListRouteId(HttpClient client)
@@ -133,6 +346,7 @@ public class CrawlControllerV2 : ControllerBase
         {
             foreach (var item in result) listRouteId.Add(item.RouteId);
         }
+
         return listRouteId;
     }
 
